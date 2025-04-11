052df1804f3faae31242f07da27be384fc00fecc
lia-oliveira <dev.gillian@gmail.com>
2025-04-10 22:29:54 -0300

    Getting orders and products

--- dsdeliver/src/dsdeliver/app/Application.java
+++ dsdeliver/src/dsdeliver/app/Application.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import db.DB;
 import dsdeliver.app.entities.Order;
@@ -18,17 +20,40 @@ public class Application {
 		
 		Statement st = conn.createStatement();
 		
-		ResultSet rs = st.executeQuery("select * from tb_order");
+		ResultSet rs = st.executeQuery("SELECT * FROM tb_order "
+				+ "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id "
+				+ "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
 		
-		while (rs.next()) {	
-			Order order = instantiateOrder(rs);			
-			System.out.println(order);
+		Map<Long, Order> map = new HashMap<>();
+		Map<Long, Product> prods = new HashMap<>();
+		
+		while (rs.next()) {
+			Long orderId = rs.getLong("order_id");
+			if (map.get(orderId) == null) {
+				Order order = instantiateOrder(rs);
+				map.put(orderId, order);
+			}
+			
+			Long productId = rs.getLong("product_id");
+			if(prods.get(productId) == null) {
+				Product p = instantiateProduct(rs);
+				prods.put(productId, p);
+			} 
+			map.get(orderId).getProducts().add(prods.get(productId));
+			
+		}
+		for (Long orderId: map.keySet()) {
+			System.out.println(map.get(orderId));
+			for (Product p : map.get(orderId).getProducts()) {
+				System.out.println(p);
+			}
+			System.out.println();
 		}
 	}
 	
 	private static Product instantiateProduct(ResultSet rs) throws SQLException {
 		Product p = new Product();
-		p.setId(rs.getLong("id"));
+		p.setId(rs.getLong("order_id"));
 		p.setDescription(rs.getString("description"));
 		p.setName(rs.getString("name"));
 		p.setImgUrl(rs.getString("image_uri"));
@@ -37,7 +62,7 @@ public class Application {
 	}
 	private static Order instantiateOrder(ResultSet rs) throws SQLException {
 		Order order = new Order();
-		order.setId(rs.getLong("id"));
+		order.setId(rs.getLong("product_id"));
 		order.setLatitude(rs.getDouble("latitude"));
 		order.setLongitude(rs.getDouble("longitude"));
 		order.setMoment(rs.getTimestamp("moment").toInstant());
